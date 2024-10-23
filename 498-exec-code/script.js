f0967e5ee5dfae664373b95ae0146f18d87e5ca7
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-10-23 06:18:48 -0300

    23OUT24 - Aula 498 - Sequência de execução de código e '.ready(...)'

---
+++ 498-exec-code/script.js
@@ -0,0 +1,35 @@
+/*
+$(document).ready(function() {
+    function teste() {
+        console.log($('#exemplo'));
+    }
+
+    teste();
+})
+*/
+
+
+
+/*
+$(function() {
+    console.log($('#exemplo'));
+});
+*/
+
+/*
+function teste() {
+    console.log($('#exemplo'));
+}
+
+$(teste);
+*/
+
+$(document).ready(function() {
+
+
+
+
+
+
+    
+})
