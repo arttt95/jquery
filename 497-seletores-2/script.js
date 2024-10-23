6f619bea3ea14e287f73fe30972c5486aff69209
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-10-23 05:56:06 -0300

    23OUT24 - Aula 497 - Selecionando elementos HTML - Parte 2

---
+++ 497-seletores-2/script.js
@@ -0,0 +1,19 @@
+// Combinação de seletores
+
+    // Selecionar elementos por classe de tag específica
+    //console.log($('input.exemplo1'));
+
+    // Selecionar elementos por classe de classe específica
+    console.log($('ul.lista1 .exemplo2'));
+
+
+// Atuação sobre o array de retorno dos elementos selecionados
+console.log($('.lista1 .exemplo2'));
+
+console.log($('.lista1 .exemplo2:first'));
+
+console.log($('.lista1 .exemplo2:last'));
+
+console.log($('.lista1 .exemplo2:even'));
+
+console.log($('.lista1 .exemplo2:odd'));
