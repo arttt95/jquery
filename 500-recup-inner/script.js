96816612aed8cf4c34f2b7a84f909b84c61e7556
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-10-24 00:39:11 -0300

    24OUT24 - Aula 500 - Recuperando e manipulando o conteúdo interno de elementos HTML ('.html()'; '.text()').

---
+++ 500-recup-inner/script.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+
+console.log('Conteúdo Div 1: ', $('#div1').html());
+
+console.log('Conteúdo Div 2: ', $('#div2').html());
+
+//console.log($('#div1').text('<strong style="color: red;">Eu sou o valor novo</strong>'));
+
+//console.log($('#div1').html('<strong style="color: red;">Eu sou o valor novo</strong>'));
+
+
+})
