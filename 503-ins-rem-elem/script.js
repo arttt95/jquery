cc037e6a06ed0349ee96047ce38f30eae6e040c5
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-10-24 03:07:36 -0300

    24OUT24 - Aula 503 - Inserindo e removendo elementos HTML (Before, After, Append, Prepend, Remove, Html).

---
+++ 503-ins-rem-elem/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    //Inserindo Elemento Inner no início Prepend
+    //$('#lista1').prepend('<li>Item 4</li>');
+    //$('#lista2').prepend('<div>Item Novo</div>');
+
+    // Inserindo Elemento Inner no final Append
+    //$('#lista1').append('<li>Item 4</li>');
+    //$('#lista2').append('<div>Item Novo</div>');
+
+    // Inserindo Elemento Não Inner Antes da Tag HTML
+    //$('#lista2').before('<p>Eu fui inserido antes e fora da Div</p>');
+    //$('#linha').before('<p>Eu fui inserido antes do Hr</p>');
+
+    // Inserindo Elemento Não Inner Após a Tag HTML
+    //$('#lista2').after('<p>Eu fui inserido após e fora da Div</p>');
+    //$('#linha').after('<p>Eu fui inserido após o Hr</p>');
+
+    // O método '.html' substitui todo o conteúdo e só serve para Tags que tem conteúdo Inner
+    //$('#lista2').html('<p>Eu substitui todo o conteúdo Inner de Lista 2</p>');
+
+    // O metodo '.remove' apaga o elemento HTML
+    //$('#linha').remove();
+    $('.item2').remove();
+
+    
+})
