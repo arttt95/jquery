6c9b3a3d427f5c925808dcda12b2868824532a53
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-10-24 03:45:00 -0300

    24OUT24 - Aula 504 - Navegando entre elementos HTML ('Parent()', 'Closest()', 'Find()').

---
+++ 504-nav-btw-elem/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+// .Closest() -> Procura por elementos pais com a Tag, Class ou Id informado:
+
+//$('.item').closest('ul');
+
+//$('.subSecao').closest('#pagina');
+
+//$('.subSecao').closest('.secao1')
+
+//$('.item').closest('#topo');
+
+///////////////////////////////////////////////////////////////////////////////
+
+// .Parent() -> Não é necessário passar nenhum parâmetro para o método,
+// ele retornará automaticamente o Elemento Pai:
+
+//$('.item').parent();
+
+//$('.secao1').parent();
+
+///////////////////////////////////////////////////////////////////////////////
+
+// .Find() -> Procura por elementos filhos com a Tag, Class ou Id informado:
+
+//$('#rodape').find('p');
+
+//$('#topo').find('.item');
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Combinando e aumentando o deslocamento dentro da DOM Tree
+
+//$('.secao1').parent().find('h1');
+
+//$('.subSecao').closest('#pagina').find('h1');
+    
+}) 
