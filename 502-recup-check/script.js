69f404b93d36504d1ebdf694ad76d95d949fefd9
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-10-24 01:27:30 -0300

    24OUT24 - Aula 502 - Recuperando e manipulando valores de checkboxes/radios e Loop com '$.each()'.

---
+++ 502-recup-check/script.js
@@ -0,0 +1,23 @@
+$(document).ready(() => {
+
+    // INPUTS RADIO
+    
+    //$('.sexo:checked')
+    //$('.sexo')
+    //$('.sexo:checked').val()
+
+    // INPUTS CHECKBOX
+
+    //$('.interesse')
+    //$('.interesse:checked')
+    //$('.interesse:checked').val() -> Retorna apenas o primeiro selecionado
+
+    /*
+    $.each($('.interesse:checked'), (indice, valor) => {
+
+        console.log(indice, valor.value)
+
+    })
+    */
+
+})
