1235bf993ec755af12e2c78181eb2a061d11d377
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-10-25 02:22:17 -0300

    25OUT24 - Aula 511 - Eventos de formulário.

---
+++ 511-forms/script.js
@@ -0,0 +1,49 @@
+$(document).ready(() => {
+
+$('#nome').focus((e) => {
+
+  console.log('Esse elemento html recebeu foco (nome)');
+  $(e.target).addClass('inputFocado');
+})
+
+$('#email').focus((e) => {
+
+  console.log('Esse elemento html recebeu foco (email)');
+  $(e.target).addClass('inputFocado');
+})
+
+$('#nome').blur((e) => {
+
+  console.log('Esse elemento html perdeu foco (nome)');
+  $(e.target).removeClass('inputFocado');
+
+})
+
+$('#email').blur((e) => {
+
+  console.log('Esse elemento html perdeu foco (email)');
+  $(e.target).removeClass('inputFocado');
+
+})
+
+$('#opcao').change((e) => {
+
+  console.log($(e.target).val());
+
+})
+
+$('#form').submit((e) => {
+
+  e.preventDefault();
+  console.log('Formulário enviado');
+
+})
+
+$('a').click((e) => {
+
+  e.preventDefault()
+
+})
+
+
+})
