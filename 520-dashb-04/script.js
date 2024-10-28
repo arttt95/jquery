b55081512911e6966f2ab57a08abfb38461c6e72
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-10-27 21:14:26 -0300

    27OUT24 - Aula 520 - Utilizando o método '$.ajax()'.

---
+++ 520-dashb-04/script.js
@@ -0,0 +1,58 @@
+$(document).ready(() => {
+ 
+    $('#documentacao').on('click', () => {
+
+        //$('#pagina').load('documentacao.html');
+
+        /*
+        $.get('documentacao.html', data => {
+            $('#pagina').html(data);
+        })
+        */
+
+        $.post('documentacao.html', data => {
+            $('#pagina').html(data);
+        })
+
+    })
+
+    $('#suporte').on('click', () => {
+
+        //$('#pagina').load('suporte.html');
+
+        /*
+        $.get('suporte.html', data => {
+            $('#pagina').html(data);
+        })
+        */
+
+        $.post('suporte.html', data => {
+            $('#pagina').html(data);
+        })
+
+    })
+
+    // Ajax
+    $('#competencia').on('change', e => {
+
+        let competencia = $(e.target).val();
+        //console.log(competencia);
+
+        $.ajax({
+            type: 'GET',
+            url: 'app.php',
+            data: `competencia=${competencia}`, //x-www-form-urlencoded -> competencia=2018-08&x=10&y=20
+            dataType: 'json',
+            success: dados => {
+                $('#numeroVendas').html(dados.numeroVendas);
+                $('#totalVendas').html(dados.totalVendas);
+            },
+            error: erro => {console.log('Erro!')}
+            
+        });
+
+        // Método, URL, Dados, Sucesso, Erro
+
+    })
+
+})
